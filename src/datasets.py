c50c5848f7b89796a954fbb44c5e5f4117469da6
daichi_yamaoka <daichi_yamaoka@bita.jp>
2020-01-15 01:09:58 +0900

    datasets.pyの作成準備

--- src/datasets.py
+++ src/datasets.py
@@ -1,12 +1,24 @@
 import os
 import json
 
-from torch.util.data import Dataset
+from torch.utils.data import Dataset
+
+
+def make_dataset():
+    pass
+
+
+def detect_face():
+    pass
 
 
 class DFDCDataset(Dataset):
     def __init__(self, data_path):
         self.data_path = data_path
+        # データの取得
+        self.data = make_dataset()
+        # 顔検出を記載
+        self.face_detected_data = detect_face()
 
     def __getitem__(self, item):
         with open(os.path.join(self.data_path, item)) as f:
